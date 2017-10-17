21e7fa52d9431748f7528cb8647d057d13f614a0
Louise Grandjonc <louve.grandjonc@gmail.com>
2017-10-17 20:20:59 +0200

    fix a few css mistakes + www.py instead of wwww.py

--- src/mnemotopy/celery.py
+++ src/mnemotopy/celery.py
@@ -1,12 +1,28 @@
-from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery
+
+import celery
+
 from django.conf import settings
 
+# set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mnemotopy.settings.prod')
+
+
+class Celery(celery.Celery):
+    def on_configure(self):
+        from raven.contrib.django.models import client
+        from raven.contrib.celery import register_signal, register_logger_signal
+
+        # register a custom filter to filter out duplicate logs
+        register_logger_signal(client)
+
+        # hook into the Celery error handler
+        register_signal(client)
+
 
 app = Celery('mnemotopy')
 
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
-
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
