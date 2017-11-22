953aeae7d809ebdbbe037fd6e0b456a311ae2b13
Louise Grandjonc <louve.grandjonc@gmail.com>
2017-11-22 09:48:49 +0100

    fix

--- src/mnemotopy/settings/local.py
+++ src/mnemotopy/settings/local.py
@@ -28,21 +28,4 @@ DATABASES = {
     }
 }
 
-
-INSTALLED_APPS += (
-    'storages',
-)
-
-AWS_STORAGE_BUCKET_NAME = 'mnemotopy-files'
-AWS_ACCESS_KEY_ID = 'AKIAIJQYOYB5ZNOKKD4Q'
-AWS_SECRET_ACCESS_KEY = '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'
-
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-STATICFILES_LOCATION = 'static'
-
-MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'mnemotopy.storages.MediaStorage'
-
 BROKER_URL = "amqp://mnemotopy:mnemotopy@localhost:5672/mnemotopy"
