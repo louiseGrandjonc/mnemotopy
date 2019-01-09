c22d15f52414e094ed4871bff68f43ec22358d50
Louise Grandjonc <louve.grandjonc@gmail.com>
2019-01-09 17:50:12 +0100

    try running with heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mnemotopy:app --log-file -
+web: gunicorn www:application --log-file -
--- www.py
+++ www.py
@@ -5,6 +5,6 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mnemotopy.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mnemotopy.settings.heroku")
 
 application = get_wsgi_application()
