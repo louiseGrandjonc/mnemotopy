866ec7239c263e954dba42e870d000ab12502bd7
Louise Grandjonc <louve.grandjonc@gmail.com>
2019-01-10 10:27:04 +0100

    fix db heroku settings

--- src/mnemotopy/settings/heroku.py
+++ src/mnemotopy/settings/heroku.py
@@ -6,7 +6,8 @@ from .base import *
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+
+DATABASES = {'default': dj_database_url.config()}
 
 INSTALLED_APPS += (
     'gunicorn',
